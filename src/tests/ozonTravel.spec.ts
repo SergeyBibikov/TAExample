fe1cf7e83f9e6ed9f4dc3cdfd195e82c57783fa3
Sergey Bibikov <sergey.bibikov.92@mail.ru>
2022-03-03 17:58:23 +0300

    Avia-Railway tickets switch

--- src/tests/ozonTravel.spec.ts
+++ src/tests/ozonTravel.spec.ts
@@ -2,8 +2,15 @@ import { test, expect } from '@playwright/test';
 import { Header } from '../pageObjects/header';
 import { Homepage } from '../pageObjects/homepage';
 
-test('Ozon Travel Link', async ({ page }) => {
+test('Header Link', async ({ page }) => {
     await Homepage.open(page);
     await Header.goToNavbarLink(page, 'Ozon Travel');
     await expect(page.locator('body')).toContainText('Покупать билеты на Ozon выгодно');
+});
+test('Avia-Railway tickets toggle ', async ({ page }) => {
+    await Homepage.open(page);
+    await Header.goToNavbarLink(page, 'Ozon Travel');
+    await expect(page.locator('body')).toContainText('Поиск дешёвых авиабилетов');
+    await page.locator('//span[text()="ЖД билеты"]/ancestor::button').click();
+    await expect(page.locator('body')).toContainText('Жд билеты на поезд');
 });
