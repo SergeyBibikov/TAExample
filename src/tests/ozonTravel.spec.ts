1dd2c3dd9d769b7881cc4fcb9bbb68db15a77ef2
SergeyBibikov <sergey.bibikov.92@mail.ru>
2022-03-04 19:42:42 +0300

    Reserve as a jur. face

--- src/tests/ozonTravel.spec.ts
+++ src/tests/ozonTravel.spec.ts
@@ -1,16 +1,25 @@
-import { test, expect } from '@playwright/test';
+import { test, expect, Page } from '@playwright/test';
 import { Header } from '../pageObjects/header';
 import { Homepage } from '../pageObjects/homepage';
 
-test('Header Link', async ({ page }) => {
+const goToTravel = async (page: Page) =>{
     await Homepage.open(page);
     await Header.goToNavbarLink(page, 'Ozon Travel');
+}
+
+test('Header Link', async ({ page }) => {
+    await goToTravel(page);
     await expect(page.locator('body')).toContainText('Покупать билеты на Ozon выгодно');
 });
 test('Avia-Railway tickets toggle ', async ({ page }) => {
-    await Homepage.open(page);
-    await Header.goToNavbarLink(page, 'Ozon Travel');
+    await goToTravel(page);
     await expect(page.locator('body')).toContainText('Поиск дешёвых авиабилетов');
     await page.locator('//span[text()="ЖД билеты"]/ancestor::button').click();
     await expect(page.locator('body')).toContainText('Жд билеты на поезд');
+});
+test('Reserve as a juridical face', async ({ page }) => {
+    await goToTravel(page);
+    await page.locator('text=Бронируйте как юрлицо').click();
+    await expect(page.locator('body')).toContainText('OZON Командировки');
+    await expect(page.locator('body')).toContainText('Уникальный сервис для оформления деловых поездок');
 });
