79af96e8b1ed29be3848df83e7c3af5cf929aee8
Sergey Bibikov <sergey.bibikov.92@mail.ru>
2022-03-24 21:00:54 +0300

    Bank client info

--- src/tests/ozonBank.spec.ts
+++ src/tests/ozonBank.spec.ts
@@ -21,10 +21,27 @@ test('Bank info. Show more', async ({ page }) => {
     await moreInfo.click();
     await expect(page.locator('body')).toContainText('Оставайтесь с нами и ждите анонсов о новых финансовых продуктах');
 });
-// TODO: Добавить отдельные кейсы для списков
-test('Client info', async ({ page }) => {
+// TODO: Добавить кейс с архивом документов
+test('For clients', async ({ page }) => {
     await page.locator('header >> text=Клиентам').click(); 
-    await expect(page.locator('body')).toContainText('Встречайте ОЗОН Банк');
+    await expect(page.locator('body')).toContainText('теперь ООО "Еком Банк" будет обслуживать операции по вашему Озон Счёту.');
     await expect(page.locator('body')).toContainText('Информация для клиентов');
     await expect(page.locator('body')).toContainText('Архив документов');
+});
+
+test('Client info docs', async ({ page }) => {
+    await page.locator('header >> text=Клиентам').click(); 
+    await page.locator('//div[text()="Информация для клиентов"]/../following-sibling::div').click();
+    const infoDocs = page.locator('//div[text()="Информация для клиентов"]/ancestor::div[contains(@class, "headerWrapper")]/..');
+    await expect(infoDocs.locator('.doc')).toHaveCount(5);
+    await expect(infoDocs)
+        .toContainText('Информация о праве потребителей финансовых услуг на направление обращения финансовому уполномоченному');
+    await expect(infoDocs)
+        .toContainText('Памятка об электронных денежных средствах');
+    await expect(infoDocs)
+        .toContainText('Правила обслуживания клиентов');
+    await expect(infoDocs)
+        .toContainText('Порядок обработки персональных данных');
+    await expect(infoDocs)
+        .toContainText('Согласие на получение рекламы');
 });
