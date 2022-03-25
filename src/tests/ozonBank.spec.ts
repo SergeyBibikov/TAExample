b56b90228a80f9f314ce04d288c90ea7196f2404
Sergey Bibikov <sergey.bibikov.92@mail.ru>
2022-03-25 17:26:24 +0300

    Bank. Docs. Archive

--- src/tests/ozonBank.spec.ts
+++ src/tests/ozonBank.spec.ts
@@ -15,22 +15,22 @@ test('Smoke', async ({ page }) => {
 test('Bank info. Show more', async ({ page }) => {
     const moreInfo = page.locator('.ob-container >> text=Узнать подробнее');
 
-    await page.locator('header >> text=О банке').click(); 
+    await page.locator('header >> text=О банке').click();
     await expect(page.locator('body')).toContainText('Ваш новый ОЗОН Банк');
     await expect(moreInfo).toHaveCount(1);
     await moreInfo.click();
     await expect(page.locator('body')).toContainText('Оставайтесь с нами и ждите анонсов о новых финансовых продуктах');
 });
-// TODO: Добавить кейс с архивом документов
+
 test('For clients', async ({ page }) => {
-    await page.locator('header >> text=Клиентам').click(); 
+    await page.locator('header >> text=Клиентам').click();
     await expect(page.locator('body')).toContainText('теперь ООО "Еком Банк" будет обслуживать операции по вашему Озон Счёту.');
     await expect(page.locator('body')).toContainText('Информация для клиентов');
     await expect(page.locator('body')).toContainText('Архив документов');
 });
 
 test('Client info docs', async ({ page }) => {
-    await page.locator('header >> text=Клиентам').click(); 
+    await page.locator('header >> text=Клиентам').click();
     await page.locator('//div[text()="Информация для клиентов"]/../following-sibling::div').click();
     const infoDocs = page.locator('//div[text()="Информация для клиентов"]/ancestor::div[contains(@class, "headerWrapper")]/..');
     await expect(infoDocs.locator('.doc')).toHaveCount(5);
@@ -44,4 +44,20 @@ test('Client info docs', async ({ page }) => {
         .toContainText('Порядок обработки персональных данных');
     await expect(infoDocs)
         .toContainText('Согласие на получение рекламы');
+});
+test('Archive docs', async ({ page }) => {
+    await page.locator('header >> text=Клиентам').click();
+    await page.locator('//div[text()="Информация для клиентов"]/../following-sibling::div').click();
+
+    const archiveDocs = page.locator('//div[text()="Архив документов"]/ancestor::div[contains(@class, "headerWrapper")]/..');
+    await expect(archiveDocs.locator('.doc')).toHaveCount(10);
+    await expect(archiveDocs).toContainText('Общие условия');
+    await expect(archiveDocs).toContainText('Тарифы РКО');
+    await expect(archiveDocs).toContainText('Правила (общие условия) выпуска и обслуживания расчетных карт с овердрафтом ООО "ОЗОН Банк"');
+    await expect(archiveDocs).toContainText('Правила обслуживания клиентов (до 24.03.2022)');
+    await expect(archiveDocs).toContainText('Правила обслуживания клиентов (до 21.03.2022)');
+    await expect(archiveDocs).toContainText('Порядок обработки персональных данных (до 24.03.2022)');
+    await expect(archiveDocs).toContainText('Порядок обработки персональных данных (до 21.03.2022)');
+    await expect(archiveDocs).toContainText('Политика обработки и защиты персональных данных (до 24.03.2022)');
+    await expect(archiveDocs).toContainText('Политика обработки и защиты персональных данных (до 21.03.2022)');
 });
