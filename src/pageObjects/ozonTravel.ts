9458bd56d4a46f77b11d00bd3f8916a1e2606195
SergeyBibikov <sergey.bibikov.92@mail.ru>
2022-05-10 10:12:04 +0300

    Find hotel test fix

--- src/pageObjects/ozonTravel.ts
+++ src/pageObjects/ozonTravel.ts
@@ -15,7 +15,7 @@ type Place = 'city' | 'hotel' | 'airport';
 async function fillPlaceField(page: Page, placeType: Place, placeName: string) {
     const loc = page.locator(OzonTravel.hotelLocators.place);
     await loc.fill(placeName);
-    await loc.click();
+    await page.waitForResponse(resp => resp.url().includes("travelHotelLocations") && resp.body().then(b => b.includes(placeName)));
 
     switch (placeType) {
         case 'city':
@@ -39,13 +39,13 @@ export class OzonTravel {
     static DESTINATION_SWITCH = searchForm + '/div/div[2]/button';
     static hotelLocators = hotel;
 
-    
-    static async open(page: Page){
+
+    static async open(page: Page) {
         await page.goto('https://www.ozon.ru/travel/')
         await page.waitForResponse('https://ozon-api.exponea.com/managed-tags/show');
     }
-    
-    static async selectHotelTab(page: Page){
+
+    static async selectHotelTab(page: Page) {
         await page.locator('//span[text()="Отели"]').click();
         await page.waitForResponse('https://ozon-api.exponea.com/managed-tags/show');
     }
@@ -57,14 +57,14 @@ export class OzonTravel {
      * @param arrivalDate 
      * @param departureDate 
      */
-    static async findHotel(page: Page, placeType: Place, place: string, arrivalDate: string, departureDate: string){
+    static async findHotel(page: Page, placeType: Place, place: string, arrivalDate: string, departureDate: string) {
         await fillPlaceField(page, placeType, place);
         await page.locator(this.hotelLocators.arrival).fill(arrivalDate);
         await page.locator(this.hotelLocators.departure).fill(departureDate);
         await page.locator(this.hotelLocators.findHotel).click();
     }
-    
-    static async findTicket(page: Page, from: string, to: string, date: Date){
+
+    static async findTicket(page: Page, from: string, to: string, date: Date) {
         const pickCity = async (city: string) => {
             await page.locator(`//div[@class="vue-portal-target"]//span[text()="${city}"]`).click();
         }
@@ -72,18 +72,18 @@ export class OzonTravel {
             await page.waitForResponse('https://www.ozon.ru/api/composer-api.bx/_action/travelMainSaveField');
         }
 
-        const month = date.toLocaleString('default', {month : 'long'});
+        const month = date.toLocaleString('default', { month: 'long' });
         const monthLoc = page.locator(`//div[span[contains(text(),"${month}")]]/..`).first();
 
         const fromLoc = page.locator(this.FROM);
         const toLoc = page.locator(this.TO);
         const dateLoc = page.locator(this.DATE);
-        const dayLoc =  monthLoc.locator(`//td//div[contains(.,"${date.getDate()}")]`).first();
+        const dayLoc = monthLoc.locator(`//td//div[contains(.,"${date.getDate()}")]`).first();
 
         await toLoc.click();
         await fromLoc.click();
         await pickCity(from);
-        await waitForField();       
+        await waitForField();
 
         await toLoc.click();
         await pickCity(to);
