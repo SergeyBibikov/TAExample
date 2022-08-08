e85570265e78914d0f30f20e72690bd96b2b536c
Sergey Bibikov <sergey.bibikov.92@mail.ru>
2022-08-08 18:48:23 +0300

    discount filter options test

--- src/tests/wildberries/searchResults.spec.ts
+++ src/tests/wildberries/searchResults.spec.ts
@@ -4,6 +4,8 @@ import WbLocators from "../../wbLocators";
 
 const FILTERS = 'div#filters'
 const DELIVERY_TIME_FILTER = '[data-filter-name="fdlvr"]'
+const DISCOUNT_FILTER = '[data-filter-name="discount"]'
+
 
 test.beforeEach(async ({ page }) => {
     await page.goto(Urls.WB_IPHONE_RESULTS);
@@ -15,9 +17,17 @@ test.afterEach(async ({ page }) => {
 
 test('Delivery time filter options', async ({ page }) => {
     const delTimeFilter = page.locator(FILTERS).locator(DELIVERY_TIME_FILTER);
-    
+
     await expect(delTimeFilter).toContainText('1 день');
     await expect(delTimeFilter).toContainText('2 дня');
     await expect(delTimeFilter).toContainText('до 3 дней');
     await expect(delTimeFilter).toContainText('до 5 дней');
+});
+
+test('Discount filter options', async ({ page }) => {
+    const delTimeFilter = page.locator(FILTERS).locator(DISCOUNT_FILTER);
+
+    await expect(delTimeFilter).toContainText('от 10% и выше');
+    await expect(delTimeFilter).toContainText('от 30% и выше');
+    await expect(delTimeFilter).toContainText('от 50% и выше');
 });
