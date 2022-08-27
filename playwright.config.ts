217acfa974b88c3a007eef3d44da8ca43a42dd22
Sergey Bibikov <sergey.bibikov.92@mail.ru>
2022-08-27 10:53:32 +0300

    less retries

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,7 +1,7 @@
 import { PlaywrightTestConfig, devices } from '@playwright/test';
 
 const config: PlaywrightTestConfig = {
-    retries: 4,
+    retries: 2,
     workers: 4,
     use: {
         screenshot: 'only-on-failure'
