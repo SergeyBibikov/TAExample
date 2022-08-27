98a939319df52cd9587a77b0cb00e8bbf55ccafb
Sergey Bibikov <sergey.bibikov.92@mail.ru>
2022-08-27 10:53:48 +0300

    waiting for load state in QA tests

--- src/pageObjects/ozonBank.ts
+++ src/pageObjects/ozonBank.ts
@@ -13,6 +13,7 @@ export class Bank {
         return this.selectors.helpSection + `//span[contains(., "${sectionName}")]/ancestor::div[contains(@class, "sliding-box")]`;
     }
     static async getAnswerCardContent(page: Page, card: string): Promise<Locator> {
+        await page.waitForLoadState('networkidle');
         const question = page.locator(this.getHelpSectionCardLocator(card));
         await question.click();
         return question.locator('div.content');
