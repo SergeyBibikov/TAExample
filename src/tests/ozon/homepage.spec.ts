209f482f69ae2267e044c1b1c5797ae0bad6ca6d
SergeyBibikov <sergey.bibikov.92@mail.ru>
2022-10-07 17:05:11 +0300

    misc links count fix

--- src/tests/ozon/homepage.spec.ts
+++ src/tests/ozon/homepage.spec.ts
@@ -83,7 +83,7 @@ test.describe('Footer', () => {
     test('Misc info links', async ({ page }) => {
         await Homepage.open(page);
         const infoLinksSection = page.locator(Footer.locators.INFO_LINKS_SECTION);
-        await expect(infoLinksSection.locator('xpath=/div')).toHaveCount(5);
+        await expect(infoLinksSection.locator('xpath=/div')).toHaveCount(4);
         await expect(infoLinksSection.locator('xpath=/div[1]/span')).toHaveText('Зарабатывайте с Ozon');
         await expect(infoLinksSection.locator('xpath=/div[2]/span')).toHaveText('О компании');
         await expect(infoLinksSection.locator('xpath=/div[3]/span')).toHaveText('Помощь');
