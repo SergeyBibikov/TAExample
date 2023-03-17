8ebdf0d24b4c8028fd47898fffb6e14d8dcb5d29
SergeyBibikov <sergey.bibikov.92@mail.ru>
2023-03-17 11:39:07 +0400

    no retries

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,7 +1,7 @@
 import { PlaywrightTestConfig, devices } from '@playwright/test';
 
 const config: PlaywrightTestConfig = {
-    retries: 2,
+    retries: 0,
     workers: 4,
     use: {
         screenshot: 'only-on-failure'
