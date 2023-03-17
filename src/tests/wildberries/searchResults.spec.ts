6e1929e4c74bc92743a774a50e21b68c624c4e33
SergeyBibikov <sergey.bibikov.92@mail.ru>
2023-03-17 11:46:20 +0400

    cleanup. wb search results

--- src/tests/wildberries/searchResults.spec.ts
+++ src/tests/wildberries/searchResults.spec.ts
@@ -21,82 +21,8 @@ test.afterEach(async ({ page }) => {
     await page.waitForTimeout(1000);
 })
 
-test('Iphone results filters list', async ({ page }) => {
-    const filters = page.locator(FILTERS);
-
-    await expect.soft(filters).toContainText('Срок доставки');
-    await expect.soft(filters).toContainText('Продавец');
-    await expect.soft(filters).toContainText('Цена');
-    await expect.soft(filters).toContainText('Скидка');
-    await expect.soft(filters).toContainText('Цвет');
-    await expect.soft(filters).toContainText('Операционная система');
-    await expect.soft(filters).toContainText('Версия операционной системы');
-    await expect.soft(filters).toContainText('Диагональ экрана');
-    await expect.soft(filters).toContainText('Модель');
-    await expect.soft(filters).toContainText('Количество SIM карт');
-    await expect.soft(filters).toContainText('Беспроводные интерфейсы');
-    await expect.soft(filters).toContainText('Объем встроенной памяти');
-    await expect.soft(filters).toContainText('Объем оперативной памяти');
-    await expect.soft(filters).toContainText('Процессор');
-    await expect.soft(filters).toContainText('Разрешение экрана');
-    await expect.soft(filters).toContainText('Спутниковая навигация');
-    await expect.soft(filters).toContainText('Стандарт связи');
-});
-
-test('Delivery time filter options', async ({ page }) => {
-    const delTimeFilter = page.locator(FILTERS).locator(DELIVERY_TIME_FILTER);
-
-    await expect.soft(delTimeFilter).toContainText('1 день');
-    await expect.soft(delTimeFilter).toContainText('2 дня');
-    await expect.soft(delTimeFilter).toContainText('до 3 дней');
-    await expect.soft(delTimeFilter).toContainText('до 5 дней');
-});
-
-test('Discount filter options', async ({ page }) => {
-    const discountFilter = page.locator(FILTERS).locator(DISCOUNT_FILTER);
-
-    await expect.soft(discountFilter).toContainText('от 10% и выше');
-    await expect.soft(discountFilter).toContainText('от 30% и выше');
-    await expect.soft(discountFilter).toContainText('от 50% и выше');
-});
-
-test('RAM filter options', async ({ page }) => {
-    const ramFilter = page.locator(FILTERS).locator(RAM_FILTER);
-
-    await expect.soft(ramFilter).toContainText('4 ГБ');
-    await expect.soft(ramFilter).toContainText('6 ГБ');
-});
-
-test('ROM filter options', async ({ page }) => {
-    const romFilter = page.locator(FILTERS).locator(ROM_FILTER);
-
-    await expect.soft(romFilter).toContainText('128 ГБ');
-    await expect.soft(romFilter).toContainText('256 ГБ');
-});
-
-test('Additional options filter has input for text model search', async ({ page }) => {
-    const input = page.locator(FILTERS).locator(ADDITIONAL_OPTIONS).locator(I_SEARCH_INPUT);
-
-    await expect(input).toHaveCount(1);
-});
-
 test('"You may also like" link presence', async ({ page }) => {
     await page.waitForSelector('a:has-text("Возможно, Вам понравится")');
-});
-
-test('Sort order options', async ({ page }) => {
-    const sortOptions = page.locator(SORT_OPTIONS_BLOCK);
-
-    await expect.soft(sortOptions).toContainText('популярности');
-    await expect.soft(sortOptions).toContainText('рейтингу');
-    await expect.soft(sortOptions).toContainText('цене');
-    await expect.soft(sortOptions).toContainText('Сначала выгодные');
-    await expect.soft(sortOptions).toContainText('обновлению');
-});
-
-test('No results on gibberish search', async ({ page }) => {
-    await page.goto(Urls.WB_GIBBERISH_RESULTS);
-    await page.waitForSelector(NO_RESULTS_FOUND);
 });
 
 test('"People also search for" text is present', async ({ page }) => {
