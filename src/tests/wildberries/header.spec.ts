9492d9043be28d07457d21495a2c4691ee6cdf9b
SergeyBibikov <sergey.bibikov.92@mail.ru>
2023-03-19 17:30:11 +0400

    wb currencies testfix

--- src/tests/wildberries/header.spec.ts
+++ src/tests/wildberries/header.spec.ts
@@ -4,9 +4,6 @@ import WbLocators from "../../wbLocators";
 
 const CART_ICON = '[data-wba-header-name="Cart"]'
 const COUNTRY_SELECTION = '[data-wba-header-name="Country"]'
-const COUNTRIES_LIST = '//div[p[text()="Выберите страну"]]'
-const CITY_BUTTON = 'span[data-wba-header-name="DLV_Adress"]'
-const DELIVERY_MAP = '//div[h2[text()="Выберите адрес доставки"]]'
 const GO_TO_MAIN_PAGE_BUTTON = '//a[text()="Перейти на главную"]'
 const SEARCH_BUTTON = 'button[data-wba-header-name="Search_text"]'
 const SEARCH_INPUT = 'input[data-wba-header-name="Search_text"]'
@@ -15,28 +12,19 @@ const SIGN_IN = '[data-wba-header-name="Login"]'
 test.beforeEach(async ({ page }) => {
     await page.goto(Urls.WB_MAIN_PAGE);
 })
-//Avoiding anti-bot threshold
-test.afterEach(async ({ page }) => {
-    await page.waitForTimeout(1000);
-})
 
-test('Location dropdown should include 7 countries', async ({ page }) => {
+test.only('Currency dropdown should include 6 currencies', async ({ page }) => {
     await page.locator(COUNTRY_SELECTION).hover();
 
-    const countriesList = page.locator(COUNTRIES_LIST)
+    const countriesList = page.locator('fieldset', { hasText: "Выберите валюту" })
 
     await expect.soft(countriesList).toBeVisible();
-    await expect.soft(countriesList).toContainText('Армения');
-    await expect.soft(countriesList).toContainText('Беларусь');
-    await expect.soft(countriesList).toContainText('Израиль');
-    await expect.soft(countriesList).toContainText('Казахстан');
-    await expect.soft(countriesList).toContainText('Киргизия');
-    await expect.soft(countriesList).toContainText('Узбекистан');
-});
-
-test('Map of delivery addresses on city button click', async ({ page }) => {
-    await page.locator(CITY_BUTTON).click();
-    await page.waitForSelector(DELIVERY_MAP);
+    await expect.soft(countriesList).toContainText('Российский рубль');
+    await expect.soft(countriesList).toContainText('Белорусский рубль');
+    await expect.soft(countriesList).toContainText('Казахстанский тенге');
+    await expect.soft(countriesList).toContainText('Армянский драм');
+    await expect.soft(countriesList).toContainText('Киргизский сом');
+    await expect.soft(countriesList).toContainText('Узбекский сум');
 });
 
 test('Small viewport header shrinkage', async ({ page }) => {
@@ -52,15 +40,6 @@ test('Small viewport header shrinkage', async ({ page }) => {
     await expect.soft(page.locator(WbLocators.WORK_AT_WB_BUTTON)).not.toBeVisible();
     await expect.soft(page.locator(SEARCH_BUTTON)).toBeVisible();
     await expect.soft(page.locator(SEARCH_INPUT)).not.toBeVisible();
-});
-
-test('Delivery info is shown on button click', async ({ page }) => {
-    await page.locator(WbLocators.DELIVERY_INFO_BUTTON).click();
-
-    const main = page.locator('main')
-
-    await expect.soft(main).toContainText('Доставка');
-    await expect.soft(main).toContainText('Информация о доставке и пунктах выдачи');
 });
 
 test('Sign in button leads to the login page', async ({ page }) => {
