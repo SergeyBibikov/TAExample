6b4a8b2bee1635f32237a19ce721cce88299e08c
SergeyBibikov <sergey.bibikov.92@mail.ru>
2023-03-19 17:39:33 +0400

    cleanup, user location spec

--- src/tests/ozon/userLocation.spec.ts
+++
@@ -1,9 +0,0 @@
-import { test, expect } from '@playwright/test';
-import { Homepage } from '../../pageObjects/homepage';
-
-test('Auto detected user location', async ({ page }) => {
-    await page.goto('https://www.ozon.ru/');
-    const currentLocation = await Homepage.getCurrentUserLocation(page);
-
-    expect(['Москва', 'Смоленск']).toContain(currentLocation);
-});
