e946825fb65e961af06fb912fedd7a1d5113b327
SergeyBibikov <sergey.bibikov.92@mail.ru>
2023-03-19 17:42:11 +0400

    railway tickets test fix

--- src/tests/ozon/ozonTravel.spec.ts
+++ src/tests/ozon/ozonTravel.spec.ts
@@ -8,7 +8,7 @@ test.describe(() => {
     test.beforeEach(async ({ page }) => {
         await page.goto(Urls.OZON_TRAVEL);
     })
-    //FIXME
+
     test('Avia tickets search form title text', async ({ page }) => {
         await expect.soft(page.locator(TRAVEL_TITLE)).toContainText('Поиск дешёвых авиабилетов');
     })
@@ -25,10 +25,10 @@ test.describe(() => {
 });
 
 
-//FIXME
 test('Railway tickets search form title text', async ({ page }) => {
     await page.goto(Urls.OZON_TRAVEL_RAILWAY);
-    await expect.soft(page.locator(TRAVEL_TITLE)).toContainText('ЖД билеты на поезд');
+
+    await expect.soft(page.locator(TRAVEL_TITLE)).toContainText('Ж/Д билеты на поезд');
 })
 
 test('Hotels search form title text', async ({ page }) => {
