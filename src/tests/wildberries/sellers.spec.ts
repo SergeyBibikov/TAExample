f37448e6e29b935ce6ef37a1e921746ad52aef5a
SergeyBibikov <sergey.bibikov.92@mail.ru>
2023-03-19 17:12:30 +0400

    cleanup.flaky

--- src/tests/wildberries/sellers.spec.ts
+++ src/tests/wildberries/sellers.spec.ts
@@ -1,53 +1,17 @@
 import { expect, test } from "@playwright/test";
 import Urls from "../../urls";
 
-const APP_STORE_BUTTON = 'a[href*="https://apps.apple.com/ru/app/wb"]'
 const PHONE_SIGN_IN_CARD = '[data-component="LoginByPhoneView"]'
 const COUNTRY_SELECT = PHONE_SIGN_IN_CARD + ' [class*="--select-locale"]'
-const PHONE_CODES_DROPDOWN = PHONE_SIGN_IN_CARD + ' ul'
 const PHONE_INPUT = PHONE_SIGN_IN_CARD + ' [data-find="phone-input"]'
 const GET_SMS_BUTTON = PHONE_SIGN_IN_CARD + '>> button:has-text("To get SMS code")'
-const GOOGLE_PLAY_BUTTON = 'a[href*="https://play.google.com/store/apps/details?id=com.wildberries.portal"]'
-const SELL_ON_WB = '[data-wba-header-name="Seller"]';
 
-//Avoiding anti-bot threshold
-test.afterEach(async ({ page }) => {
-    await page.waitForTimeout(1000);
-})
-
-test('Sell on Wildberries button click leads to sellers page', async ({ page, context }) => {
-    await page.goto(Urls.WB_MAIN_PAGE);
-    await page.locator(SELL_ON_WB).click();
-    const newPage = await context.waitForEvent('page');
-    await expect(newPage.locator('body')).toContainText('Collaboration with Wildberries');
-});
-
-test.describe('Seller page', () => {
-    test.beforeEach(async ({ page }) => {
-        await page.goto(Urls.WB_SELLER_PAGE);
-    });
-
-    test('Header info links presence', async ({ page }) => {
-        await expect.soft(page.locator('header')).toContainText('About the company');
-        await expect.soft(page.locator('header')).toContainText('About the service');
-        await expect.soft(page.locator('header')).toContainText('Collaboration');
-    });
-
-    test('Phone sign in component elements', async ({ page }) => {
-        await page.waitForSelector(PHONE_SIGN_IN_CARD);
-        await expect.soft(page.locator(PHONE_SIGN_IN_CARD)).toContainText('Country');
-        await expect.soft(page.locator(PHONE_SIGN_IN_CARD)).toContainText('Сontact number');
-        await expect.soft(page.locator(COUNTRY_SELECT)).toHaveCount(1);
-        await expect.soft(page.locator(PHONE_INPUT)).toHaveCount(1);
-        await expect.soft(page.locator(GET_SMS_BUTTON)).toHaveCount(1);
-    });
-
-    test('Country phone codes dropdown', async ({ page }) => {
-        await page.waitForSelector(PHONE_SIGN_IN_CARD);
-        await page.locator(COUNTRY_SELECT).click();
-        await expect.soft(page.locator(PHONE_CODES_DROPDOWN)).toContainText('+375');
-        await expect.soft(page.locator(PHONE_CODES_DROPDOWN)).toContainText('+7');
-        await expect.soft(page.locator(PHONE_CODES_DROPDOWN)).toContainText('+996');
-        await expect.soft(page.locator(PHONE_CODES_DROPDOWN)).toContainText('+374');
-    });
+test('Seller page Phone sign in component elements', async ({ page }) => {
+    await page.goto(Urls.WB_SELLER_PAGE);
+    await page.waitForSelector(PHONE_SIGN_IN_CARD);
+    await expect.soft(page.locator(PHONE_SIGN_IN_CARD)).toContainText('Country');
+    await expect.soft(page.locator(PHONE_SIGN_IN_CARD)).toContainText('Сontact number');
+    await expect.soft(page.locator(COUNTRY_SELECT)).toHaveCount(1);
+    await expect.soft(page.locator(PHONE_INPUT)).toHaveCount(1);
+    await expect.soft(page.locator(GET_SMS_BUTTON)).toHaveCount(1);
 })
